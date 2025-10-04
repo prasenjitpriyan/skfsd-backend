44029d96b0ab500069346064bdca573e2d7b2771
prasenjitpriyan <prasenjit.webdev@icloud.com>
2025-10-04 21:22:31 +0530

    Basic structure added

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+//import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config({ path: '.env.local' });
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+//app.use("/api/users", userRoutes);
+
+app.get('/', (req, res) => {
+  res.send('SKFSD Backend is running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
